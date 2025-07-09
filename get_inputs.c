e362c1d5982959724c24a23f78564e1639b6cc86
Joseph Hughes <joeydarlo1000@gmail.com>
2025-07-09 23:11:25 +0000

    finally implemented get_string. this took way to long

--- get_inputs.c
+++ get_inputs.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char *get_string() {
-  char *data;
-  scanf("%s", data);
-  return data;
+  char *buffer = malloc(50);
+  int current_length = 0;
+
+  while (1) {
+    fgets(buffer + current_length, 11, stdin);
+    int chunk_len = strlen(buffer + current_length);
+    current_length += chunk_len;
+
+    if (buffer[current_length - 1] != '\n') {
+      buffer = realloc(buffer, current_length + 50);
+    }
+    if (buffer[current_length - 1] == '\n') {
+      break;
+    }
+  }
+  return buffer;
 }
