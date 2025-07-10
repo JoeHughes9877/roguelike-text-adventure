fa2c77ca0d4cdec84726d8647d37203c77837117
Joseph Hughes <joeydarlo1000@gmail.com>
2025-07-10 17:36:14 +0000

    added db connection

--- scr/header.h
+++ scr/header.h
@@ -1,6 +1,14 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+#include <sqlite3.h>
+
+// get_inputs.c
 char *get_string();
+
+// database
+extern sqlite3 *db;
+
+// Functions for database handling
 
 #endif
