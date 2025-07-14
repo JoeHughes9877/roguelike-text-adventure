c20bfac7122a0a9fff77d273104d9ffbe6d3781b
Joseph Hughes <joeydarlo1000@gmail.com>
2025-07-14 19:01:40 +0000

    implemented dynamic array needed for removing values mid game.

---
+++ scr/vector.c
@@ -0,0 +1,62 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct vector {
+  int size;
+  int capacity;
+  char *elements[];
+};
+
+void free_vector(struct vector *vec);
+
+void delete_by_value(int new_size, struct vector *vec, char *target_value) {
+  int current_max_index = vec->size - 1;
+
+  char **temp_storage = malloc(new_size * sizeof(char *));
+
+  for (int index = 0; index < new_size; index++) {
+    if (strcmp(vec->elements[index], target_value) != 0) {
+      continue;
+    } else {
+      memcpy(&vec[index], &temp_storage[index], sizeof(int));
+    }
+  }
+
+  vec->capacity = new_size;
+
+  if (current_max_index > new_size) {
+    current_max_index = new_size;
+  }
+
+  free_vector(vec);
+}
+
+void resize(int new_size, struct vector *vec) {
+  int current_max_index = vec->size - 1;
+
+  char **temp_storage = malloc(new_size * sizeof(char *));
+
+  for (int index = 0; index < new_size; index++) {
+    memcpy(&vec[index], &temp_storage[index], sizeof(int));
+  }
+
+  vec->capacity = new_size;
+
+  if (current_max_index > new_size) {
+    current_max_index = new_size;
+  }
+
+  free_vector(vec);
+}
+
+void free_vector(struct vector *vec) {
+  if (!vec) {
+    return;
+  }
+
+  for (int i = 0; i < vec->size; i++) {
+    free(vec->elements[i]);
+  }
+  free(vec);
+}
