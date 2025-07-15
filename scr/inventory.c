6cb04b9fc4d4cf74b6cd12a51ec9cb3ba96904a2
Joseph Hughes <joeydarlo1000@gmail.com>
2025-07-15 20:17:40 +0000

    changed add to inventory so it took from elements as its now a vector

--- scr/inventory.c
+++ scr/inventory.c
@@ -1,11 +1,13 @@
 #include "inventory.h"
-#include "header.h"
+#include "database.h"
 #include "vector.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 struct vector *inventory = NULL;
+
+const int INVENTORY_SIZE = 8;
 
 void print_inventory() {
   if (inventory->size == 0) {
@@ -34,10 +36,11 @@ void add_to_inventory(char *players_input) {
   }
 
   for (int i = 0; i < items.num_items_in_room; i++) {
-    if (strstr(players_input, items.name[i]) != NULL) {
-      inventory->elements[inventory->size] = strdup(items.name[i]);
+    if (strstr(players_input, items.name->elements[i]) != NULL) {
+      inventory->elements[inventory->size] = strdup(items.name->elements[i]);
 
-      printf("You carefully stow the %s into your pack.\n", items.name[i]);
+      printf("You carefully stow the %s into your pack.\n",
+             items.name->elements[i]);
       inventory->size++;
       return;
     }
@@ -49,7 +52,8 @@ void check_if_in_inventory() {}
 void remove_item() {}
 
 struct vector *init_inventory() {
-  struct vector *inventory = malloc(sizeof(struct vector) + 8 * sizeof(char *));
+  struct vector *inventory =
+      malloc(sizeof(struct vector) + INVENTORY_SIZE * sizeof(char *));
   inventory->size = 0;
   inventory->capacity = 8;
 
