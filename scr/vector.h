ee484cbace41fee6d252833049d909995b8f3d87
Joseph Hughes <joeydarlo1000@gmail.com>
2025-07-16 00:05:04 +0000

    added funcs

--- scr/vector.h
+++ scr/vector.h
@@ -7,4 +7,9 @@ struct vector {
   char *elements[];
 };
 
+void print_vector(struct vector *vec);
+void free_vector(struct vector *vec);
+void resize(int new_size, struct vector *vec);
+void delete_by_value(struct vector *vec, char *target_value);
+
 #endif
