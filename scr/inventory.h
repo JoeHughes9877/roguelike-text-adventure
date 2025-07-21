da414b98e9b7e93db35b0c9d553cc92ec6a27ad7
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-07-21 14:27:49 +0000

    changed func name

--- scr/inventory.h
+++ scr/inventory.h
@@ -8,7 +8,7 @@ extern struct vector *inventory;
 void add_to_inventory(char *input);
 void print_inventory();
 void check_if_in_inventory();
-void remove_item(char *players_input);
+void remove_item_from_inventory(char *players_input);
 struct vector *init_inventory();
 
 #endif
