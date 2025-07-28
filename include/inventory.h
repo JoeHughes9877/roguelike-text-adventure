6e90f1e542d5d747d6cfda07bd9d812ed0c1e4b2
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-07-28 23:43:20 +0100

    added function calls

--- include/inventory.h
+++ include/inventory.h
@@ -2,13 +2,14 @@
 #define INVENTORY_H
 
 #include "vector.h"
+#include <stdbool.h>
 
 extern struct vector *inventory;
 
 void add_to_inventory(char *input);
 void print_inventory();
-void check_if_in_inventory();
-void remove_item_from_inventory(char *players_input);
+void remove_item_from_inventory(char *players_input, bool is_dropping);
 struct vector *init_inventory();
+int check_if_in_inventory(char *item);
 
 #endif
--- include/items.h
+++ include/items.h
@@ -1,9 +1,15 @@
 #ifndef ITEMS_H
 #define ITEMS_H
 
-#include <stdio.h>
+typedef void (*func_ptr)(void);
 
-// Weapon Functions
+typedef struct {
+  const char *name;
+  const char *type;
+  func_ptr func;
+} FunctionMap;
+
+// Function declarations
 void use_iron_longsword();
 void use_iron_greatsword();
 void use_shiv();
@@ -11,7 +17,6 @@ void use_iron_dagger();
 void use_rusty_sword();
 void use_steel_mace();
 
-// Armor Functions
 void equip_leather_cuirass();
 void equip_fine_steel_helmet();
 void equip_prisoners_ragged_trousers();
@@ -20,7 +25,6 @@ void equip_imperial_guard_shield();
 void equip_studded_leather_armor();
 void equip_wooden_shield();
 
-// Consumable Functions
 void drink_potion_of_healing();
 void drink_minor_healing_potion();
 void drink_grand_healing_potion();
@@ -30,7 +34,6 @@ void eat_sweetroll();
 void eat_moldy_bread();
 void use_welkynd_stone();
 
-// Tool Functions
 void use_lockpick();
 void use_master_lockpick();
 void use_torch();
@@ -38,7 +41,6 @@ void use_rusty_shovel();
 void use_spyglass();
 void use_bear_trap();
 
-// Scroll Functions
 void read_scroll_of_fireball();
 void read_scroll_of_ice_storm();
 void read_scroll_of_fire_storm();
@@ -46,30 +48,28 @@ void read_scroll_of_lightning_bolt();
 void read_scroll_of_healing_hands();
 void read_scroll_of_detect_life();
 
-// Quest Item Functions
 void examine_rusty_key();
 void examine_journal_entry();
 void examine_old_map_fragment();
 void examine_worn_leather_map();
 
-// Currency Functions
 void pick_up_gold_coin();
 void pick_up_bag_of_gold_small();
 void pick_up_bag_of_gold_large();
 void pick_up_ancient_coin();
 
-// Jewelry Functions
 void wear_silver_ring();
 void wear_enchanted_amulet();
 
-// Book Functions
 void read_lockpicking_guide();
 
-// Misc/Silly Item Functions
 void interact_waterlogged_book();
 void interact_empty_wine_bottle();
 void interact_glowing_mushroom();
 void interact_broken_lantern();
 void interact_lucky_rabbits_foot();
+void use_item(char *item_name);
 
-#endif // ITEMS_H\
+extern FunctionMap function_map[];
+
+#endif
