2cb5956a10364cf8eae17ba55946e4029f5e5879
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-07-30 08:20:19 +0100

    locate enemy in room

--- src/utils.c
+++ src/utils.c
@@ -60,3 +60,13 @@ int generate_random_number_bias(int min_value, int max_value, int bias) {
 
   return rand() % (max_value - min_value + 1);
 }
+
+Enemy *locate_enemy(char *player_input) {
+  for (int i = 0; i < num_of_enemies_in_room; i++) {
+    if (strstr(player_input, (char *)enemies_in_room[i]) != NULL) {
+      return enemies_in_room[i];
+    }
+  }
+  printf("enemy not found in the room.\n");
+  return NULL;
+}
