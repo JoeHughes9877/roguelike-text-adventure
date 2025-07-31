d0bb6005d6f8f84af47ce65994c7825c13faef82
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-07-31 11:28:14 +0100

    point to the name, silly me

--- src/utils.c
+++ src/utils.c
@@ -63,7 +63,7 @@ int generate_random_number_bias(int min_value, int max_value, int bias) {
 
 Enemy *locate_enemy(char *player_input) {
   for (int i = 0; i < num_of_enemies_in_room; i++) {
-    if (strstr(player_input, (char *)enemies_in_room[i]) != NULL) {
+    if (strstr(player_input, (char *)enemies_in_room[i]->name) != NULL) {
       return enemies_in_room[i];
     }
   }
