37d462f31a1f7cf01df695b45778dfdd867186e9
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-01 16:42:25 +0100

    added destroy room contents, to stop seg err

--- include/room.h
+++ include/room.h
@@ -8,5 +8,6 @@ void look_around_room();
 void remove_item_from_room(char *item);
 void generate_exits_in_room(char *entry_direction);
 int can_go_dir(char *direction);
+void destroy_room_contents();
 
 #endif
