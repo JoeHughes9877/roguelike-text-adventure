281918c54708623d476ceee5daa2721c9fcdf212
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-01 21:44:09 +0100

    made in comabt and enemy pointer global

--- include/combat.h
+++ include/combat.h
@@ -3,8 +3,11 @@
 
 #include "../include/entity.h"
 
+extern bool in_combat;
+extern Enemy *enemy_pointer;
+
 // Function declarations
-void attack_enemy(Entity *ent_one, Entity *ent_two);
+void attack_roll(Entity *ent_one, Entity *ent_two, int damage);
 void defend(Entity *ent_one, Entity *ent_two);
 void Flee(Entity *ent);
 void combat_loop();
