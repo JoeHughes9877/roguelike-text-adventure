165395f25ce955386697fe60db28570d7eee700e
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-01 16:42:42 +0100

    changed del by index as it was causing an err

--- include/vector.h
+++ include/vector.h
@@ -7,9 +7,8 @@ struct vector {
   char **elements;
 };
 
-void resize_vector(int newSize, struct vector *vec);
 void print_vector(struct vector *vec);
 void free_vector(struct vector *vec);
 void delete_by_value(struct vector *vec, char *target_value);
-struct vector *delete_by_index(struct vector *vec, int index);
+void delete_by_index(struct vector *vec, int index);
 #endif
--- src/vector.c
+++ src/vector.c
@@ -7,7 +7,7 @@
 
 void free_vector(struct vector *vec);
 void resize(int newSize, struct vector *vec);
-struct vector *delete_by_index(struct vector *vec, int index);
+void delete_by_index(struct vector *vec, int index);
 
 void delete_by_value(struct vector *vec, char *target_value) {
   for (int i = 0; i < vec->size; i++) {
@@ -18,31 +18,6 @@ void delete_by_value(struct vector *vec, char *target_value) {
       vec->size--;
       break;
     }
-  }
-}
-
-void resize_vector(int newSize, struct vector *vec) {
-  char **temp = malloc((newSize) * sizeof(char *));
-
-  if (!temp) {
-    printf("malloc failed. ");
-    return;
-  }
-
-  for (int i = 0; i < vec->size; i++) {
-    temp[i] = vec->elements[i];
-  }
-
-  for (int i = 0; i < vec->size; i++) {
-    free(vec->elements[i]);
-  }
-  free(vec->elements);
-
-  vec->elements = temp;
-  vec->capacity = newSize;
-
-  if (vec->size > newSize) {
-    vec->size = newSize;
   }
 }
 
@@ -63,11 +38,11 @@ void print_vector(struct vector *vec) {
   }
 }
 
-struct vector *delete_by_index(struct vector *vec, int index) {
+void delete_by_index(struct vector *vec, int index) {
 
   if (index < 0 || index >= vec->size) {
     printf("Invalid index %d! Nothing to delete.\n", index);
-    return vec;
+    return;
   }
 
   free(vec->elements[index]);
@@ -75,25 +50,12 @@ struct vector *delete_by_index(struct vector *vec, int index) {
   for (int j = index; j < vec->size - 1; j++) {
     vec->elements[j] = vec->elements[j + 1];
   }
-  char **temp = NULL;
   vec->size--;
 
-  if (vec->size > 0 && vec->elements != NULL) {
-    vec->elements[vec->size] = NULL;
-  }
-  if (vec->size == 0) {
+  if (vec->size > 0) {
+    vec->elements = realloc(vec->elements, sizeof(char *) * vec->size);
+  } else {
     free(vec->elements);
-  } else {
-    temp = realloc(vec->elements, vec->size * sizeof(char *));
-    if (!temp) {
-      return vec;
-    }
-  }
-
-  if (temp == NULL) {
-    return vec;
-  } else {
-    vec->elements = temp;
-    return vec;
+    vec->elements = NULL;
   }
 }
